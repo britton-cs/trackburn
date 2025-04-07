74902a65c6ce53001d36658ba9204d4d456051bf
Britton Engleman <britton.engleman@snhu.edu>
2025-04-06 20:29:37 -0400

    Added vehicle system

---
+++ Source/trackburn_proj/ArcadeVehiclePawn.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WheeledVehiclePawn.h"
+#include "ArcadeVehiclePawn.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TRACKBURN_PROJ_API AArcadeVehiclePawn : public AWheeledVehiclePawn
+{
+	GENERATED_BODY()
+	
+};
